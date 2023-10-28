8f43e7d6732cd4e65d43cbb6097ff2478f4c2f56
Andrey <94688521+Andrew300189@users.noreply.github.com>
2023-10-28 23:55:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './components/store';
+
 import App from './components/Application/App';
 
-
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
